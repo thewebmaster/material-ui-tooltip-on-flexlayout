e4486a81d91d14c69841d36658d35771c268c4b7
Christian Turno <christianjayco@turno.co.nz>
2021-05-12 22:50:53 +1200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# material-ui-tooltip-on-flexlayout
+# How to replicate the issue
+
+1. Clone the project from github
+
+```Bash
+git clone git@github.com:thewebmaster/material-ui-tooltip-on-flexlayout.git
+```
+
+2. Once cloned, go to the project directory and run `npm install` and then `npm start` and the project will start and open up your browser to `http://localhost:3000/`
+
+3. Test material tooltip on `Hover here...` text. You should be able to confirm that the material tooltip at this state is working.
+
+![Image of Demo 1](https://github.com/thewebmaster/material-ui-tooltip-on-flexlayout/blob/master/demo/demo1.png?raw=true)
+
+4. But if you open the `Floatable Panel` into a `new window` and hover over the `Hover here...` text, you'll see that the tooltip still appears from the original docked panel.
+
+![Image of Demo 2](https://github.com/thewebmaster/material-ui-tooltip-on-flexlayout/blob/master/demo/demo2.png?raw=true)
+
+![Image of Demo 3](https://github.com/thewebmaster/material-ui-tooltip-on-flexlayout/blob/master/demo/demo3.png?raw=true)
---
+++ src/App.tsx
@@ -0,0 +1,121 @@
+import React, { useState } from 'react';
+import FlexLayout from "flexlayout-react";
+import './App.css';
+import 'flexlayout-react/style/gray.css'
+import TabNode from 'flexlayout-react/declarations/model/TabNode';
+import { DropResult, ResponderProvided } from 'react-beautiful-dnd';
+import { createStyles, withStyles, Theme, WithStyles } from '@material-ui/core/styles';
+import { Tooltip } from '@material-ui/core';
+
+const style = (theme: Theme) => createStyles({
+  panelLeft: {
+    background: 'linear-gradient(to right, rgba(255,255,255,0.75) 0%, rgba(255,255,255,0.75) 70%, rgba(255,255,255,0.1) 100%)',
+    bottom: 0,
+    left: 0,
+    paddingLeft: 2,
+    position: 'absolute',
+    top: 0,
+  },
+  sideButton: {
+    '& .circle': {
+      background: 'rgba(255, 255, 255, 0.75)',
+      borderRadius: '50%',
+      color: 'rgba(0, 0, 0, 0.54)',
+      height: '3rem',
+      padding: theme.spacing(1.5),
+      width: '3rem',
+    },
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  panelHost: {
+    position: 'relative',
+  },
+  document: {
+    '& .draggable': {
+    },
+    '&:not(:last-child)': {
+      borderBottom: '1px solid rgba(224, 224, 224, 1)',
+    },
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  draggableContent: {
+    alignItems: 'center',
+    background: theme.palette.background.paper,
+    display: 'inline-flex',
+    height: 100,
+    justifyContent: 'center',
+    margin: 1,
+    minWidth: 300,
+  },
+});
+
+const json = {
+  global: {},
+  borders: [],
+  layout:{
+    "type": "row",
+    "weight": 100,
+    "children": [
+      {
+        "type": "tabset",
+        "weight": 50,
+        "selected": 0,
+        "children": [
+          {
+            "type": "tab",
+            "name": "Floatable Panel with Tooltip",
+            "component": "floatable",
+            "enableFloat": true,
+          }
+        ]
+      },
+      {
+        "type": "tabset",
+        "weight": 50,
+        "selected": 0,
+        "children": [
+          {
+            "type": "tab",
+            "name": "Default Panel",
+            "component": "default",
+          }
+        ]
+      }
+    ]
+  }
+};
+
+const App: React.FunctionComponent<WithStyles> = ({ classes }) => {
+  const [model] = useState(FlexLayout.Model.fromJson(json));
+
+  const onDragEnd = (result: DropResult, provided: ResponderProvided): void => {
+    
+    console.log(result);
+    console.log(provided);
+
+    return;
+  }
+
+  const factory = (node: TabNode) => {
+    const component = node.getComponent();
+    if (component === "floatable") {
+      return <div>
+        <div className={classes.document}>
+          <Tooltip title={`This is a sample tooltip.`} disableFocusListener={true} placement={`top-start`}>
+            <p>Hover over here..</p>
+          </Tooltip>
+        </div>
+      </div>;
+    } else if (component === "default") {
+      return <div><p>This is a default panel</p></div>;
+    }
+  }
+  return (
+    <FlexLayout.Layout model={model} factory={factory} supportsPopout={true} />
+  );
+}
+
+export default  withStyles(style)(App);
